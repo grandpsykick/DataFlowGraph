ba513d28801f5c686dce9c9fc7c82b694ed47a63
GrandpsyKick <grandpsykick@users.noreply.github.com>
2017-05-23 12:29:09 -0400

    Adding some comments to main.cpp just for cleanliness

--- FlowGraph/main.cpp
+++ FlowGraph/main.cpp
@@ -3,10 +3,13 @@
 
 int main(int argc, char * argv[])
 {
+    // Initializing the Qt application event system
     QApplication app(argc, argv);
 
+    // Creating the flow graph main window
     FlowGraphWindow flowWindow;
     flowWindow.show();
 
+    // Executing the event system
     return app.exec();
 }
